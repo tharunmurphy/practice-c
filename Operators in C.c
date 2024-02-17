ea7d76144051842e35ed072f299e0d4347cf3606
Tharunmurphy <tharunlahtp@gmail.com>
2024-02-17 17:16:41 +0530

    added operators

---
+++ Operators in C.c
@@ -0,0 +1,30 @@
+/*C Operators:
+Arithmetic 
++ - * / %
+/ is for quotient
+% is for remainder (known as modulo operator)
+
+Bitwise (produces a new number
+based on two given numbers)
+AND  & 
+OR   |
+XOR  ^
+Leftshift <<
+Rightshift >> 
+
+Relational or Comparison Operators [compares 
+two values and returns true or false(boolean)]
+Less than                      <
+Greater than                   >
+Less than or equal to          <=
+Greater than or equal to       >=
+equal to                       ==
+not equal to                   !=
+
+Logical Operators [Checks two variables for 
+non zero values and outputs true or false 
+based on logic gates(boolean)]
+AND       && 
+OR        ||
+NOT       !()
+*/
